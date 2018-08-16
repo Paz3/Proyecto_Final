b0c9a22e80c1fcb82deceb463ba9b19d764173b5
Paz Romero <paz.romero.soto@gmail.com>
2018-08-16 18:37:34 -0300

    Séptimo commit

---
+++ Paginaweb/assets/js/script.js
@@ -0,0 +1,9 @@
+// $(function() {
+
+// $('.navbar-nav').append(`
+//             <li><a href="sobremi.html">Sobre mí</a></li>
+//             <li><a href="portafolio.html">Portafolio</a></li>
+//             <li><a href="clima.html">Clima</a></li>
+//             `)
+
+// });
